394b2c0941ba0cf6756db3b05f3f1135b592358c
김도형 <kdobro92@gmail.com>
2024-07-28 22:08:44 +0900

    kakaomap error with script tag

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,25 +1,25 @@
-import { Inter } from "next/font/google";
-import Script from 'next/script'
-import Head from "next/head";
-import "./globals.css";
+import { Inter } from 'next/font/google'
+// import Script from 'next/script'
+import Head from 'next/head'
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "룩맨즈헤어",
-  description: "LOOKMANS hair project",
-};
+  title: '룩맨즈헤어',
+  description: 'LOOKMANS hair project',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <Script
+        <script
           src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.KAKAO_APP_KEY}&libraries=services,clusterer,drawing`}
           strategy="beforeInteractive"
         />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
