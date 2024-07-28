bf438c93e313fb9d58c923b91f9514dfd9d00c03
김도형 <kdobro92@gmail.com>
2024-07-28 21:57:07 +0900

    kakaomap error

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ]
+  },
   env: {
-    KAKAO_APP_KEY: process.env.NEXT_PUBLIC_KAKAO_API_KEY
-  }
-};
+    KAKAO_APP_KEY: process.env.NEXT_PUBLIC_KAKAO_API_KEY,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
